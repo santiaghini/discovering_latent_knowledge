09005aa199b4cebd375058b7460b3ac83611aff4
Santiago Hernández <santi.hergut@gmail.com>
2023-03-16 20:40:24 -0700

    print loss

--- ccs.py
+++ ccs.py
@@ -87,8 +87,9 @@ class CCS(object):
         # informative_loss = ((1 - torch.max(p0, torch.max(p1, torch.max(p2, p3))))**2).mean(0)
         # informative_loss = ((1 - torch.max(p0, torch.max(p1, torch.max(p2, p3))) + torch.min(p0, torch.min(p1, torch.min(p2, p3))))**2).mean(0)
         # informative_loss = ((1 - torch.max(p0, torch.max(p1, torch.max(p2, p3))))**2).mean(0)
-        informative_loss = ((1 - torch.max(p0, torch.max(p1, torch.max(p2, p3))))**2).mean(0)
-        # informative_loss = ((-(p0*torch.log(p0) + p1*torch.log(p1) + p2*torch.log(p2) + p3*torch.log(p3)))**2).mean(0)
+        # informative_loss = ((1 - torch.max(p0, torch.max(p1, torch.max(p2, p3))))**2).mean(0)
+        informative_loss = ((-(p0*torch.log(p0) + p1*torch.log(p1) + p2*torch.log(p2) + p3*torch.log(p3)))**2).mean(0)
+        print("informative_loss: {informative_loss}")
         consistent_loss = (((p0 + p1 + p2 + p3) - 1)**2).mean(0)
         # TODO: play with weighting if it doesnt work. Try a grid
         # downweighting consistency loss, not too much, or upweighting
