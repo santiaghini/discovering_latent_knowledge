59a303889a3388d01780075ef0dc4c28897cd2d9
Santiago Hernández <santi.hergut@gmail.com>
2023-03-16 21:16:04 -0700

    clean prints

--- ccs.py
+++ ccs.py
@@ -179,9 +179,7 @@ class CCS(object):
         for train_num in range(self.ntries):
             self.initialize_probe()
             loss = self.train()
-            print(f"loss for iter: {loss}")
             if loss < best_loss:
-                print(f"better loss achieved")
                 self.best_probe = copy.deepcopy(self.probe)
                 best_loss = loss
 
